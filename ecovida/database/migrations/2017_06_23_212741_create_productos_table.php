ce5af72a2676aabd90dfae47719ee2374f650285
Chichar <richard.sancho@ucrso.info>
2017-06-23 16:35:44 -0600

    Creado migrate de Productos

---
+++ ecovida/database/migrations/2017_06_23_212741_create_productos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+
+class CreateProductosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('Producto', function (Blueprint $table){
+			$table->increments('id');
+			$table->string('tipo',150);
+			$table->string('nombre',150);
+			$table->text('caracteristicas');
+			$table->integer('precio');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::dropIfExists('Producto');
+	}
+
+}
